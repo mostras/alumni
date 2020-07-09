f6acc2bb7f2635903c7c8689a47efa7f9fb3ae1b
Adrien Barbier <adrien-barbier@hotmail.fr>
2020-07-09 09:56:52 +0200

    Writing test for create user

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,38 @@
 require 'rails_helper'
 
-RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe User, :type => :model do
+
+  before(:all) do
+    @user1 = create(:user)
+  end
+
+  it "is valid with valid attributes" do
+    expect(@user1).to be_valid
+  end
+
+  it "is not valid without an email" do
+    user2 = build(:user, email: nil)
+    expect(user2).to_not be_valid
+  end
+
+  it "has a unique email" do
+    user2 = build(:user, email: "joe@gmail.com")
+    expect(user2).to_not be_valid
+  end
+
+  it "is not valid without a password" do
+    user2 = build(:user, password: nil)
+    expect(user2).to_not be_valid
+  end
+
+  it "is not valid without a first_name" do
+    user2 = build(:user, first_name: nil)
+    expect(user2).to_not be_valid
+  end
+
+  it "is not valid without a last_name" do
+    user2 = build(:user, last_name: nil)
+    expect(user2).to_not be_valid
+  end
+
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,14 +1,16 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
+ENV['RAILS_ENV'] ||= 'test'
+require File.expand_path('../config/environment', __dir__)
+
 require 'spec_helper'
 require 'rspec/rails'
 require 'devise'
 require_relative 'support/controller_macros'
+require 'support/factory_bot'
 
-ENV['RAILS_ENV'] ||= 'test'
-require File.expand_path('../config/environment', __dir__)
+
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
-require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
