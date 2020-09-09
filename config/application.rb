f04cc3c9d4be300a524e895b87d8f60ac11fbd72
Adrien Barbier <adrien-barbier@hotmail.fr>
2020-09-09 10:41:34 +0200

    Add translation for devise

--- config/application.rb
+++ config/application.rb
@@ -13,6 +13,9 @@ module Alumni
 
     config.active_job.queue_adapter = :sidekiq
 
+    config.i18n.available_locales = [:fr, :en]
+    config.i18n.default_locale = :fr
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
