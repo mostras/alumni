da090bb4d6c351bd9258462d661510930bbd28f8
Adrien Barbier <adrien-barbier@hotmail.fr>
2020-09-11 09:10:24 +0200

    Cleaning code of the feature: add url linkedin to the google sheet

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,10 +7,16 @@ class User < ApplicationRecord
   has_many :diplomas
   has_many :jobs
 
+  after_create :add_mail_to_google_sheet
+
   # validates :first_name, presence: true
   # validates :last_name, presence: true
 
   def full_name
     "#{first_name} #{last_name}"
+  end
+
+  def add_mail_to_google_sheet
+    AddMail.new(id, linkedin_url).google_sheet
   end
 end
