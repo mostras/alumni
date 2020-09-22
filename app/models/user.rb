a51d8ece5ae86d73669c5dd66c8f097bf7a13a1e
Adrien Barbier <adrien-barbier@hotmail.fr>
2020-09-22 08:25:11 +0200

    Updating style of the registration

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ApplicationRecord
   has_one_attached :photo
   has_many :tags
   has_many :user_sectors
-  validates_length_of :user_sectors, maximum: 2
+  validates_length_of :user_sectors, maximum: 5
   has_many :work_experiences, dependent: :destroy
   has_many :school_experiences, dependent: :destroy
 
--- app/models/user_sector.rb
+++ app/models/user_sector.rb
@@ -3,14 +3,7 @@ class UserSector < ApplicationRecord
   belongs_to :sector
   validates_associated :user
 
-  # validate :check_sum_sector
 
-  private
 
-  # def check_sum_sector
-  #   if user.user_sectors.size >= 5
-  #     errors.add :base, 'There can only be one global setting/your message here'
-  #   end
-  # end
 
 end
