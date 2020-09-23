0c1cb3921a5a6d9b18135125268b703da8ecb7a3
Adrien Barbier <adrien-barbier@hotmail.fr>
2020-09-23 08:43:27 +0200

    Add validation on first name and last name

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,10 +12,8 @@ class User < ApplicationRecord
   has_many :school_experiences, dependent: :destroy
 
 
-
-
-  # validates :first_name, presence: true
-  # validates :last_name, presence: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
 
   def full_name
     "#{first_name} #{last_name}"
