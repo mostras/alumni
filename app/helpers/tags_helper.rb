bfa84fea470a8c8bf73bf7a88a190fdcac1d1af8
Adrien Barbier <adrien-barbier@hotmail.fr>
2020-09-28 22:41:07 +0200

    Working on the logic to display the current job or the currend school

--- app/helpers/tags_helper.rb
+++ app/helpers/tags_helper.rb
@@ -1,21 +1,32 @@
 module TagsHelper
   def last_activity(student)
-    if student.work_experiences.any?
-      last_work_experience = student.work_experiences.order(:end_time).last
-    end
 
-    if student.school_experiences.any?
-      last_school_experience = student.school_experiences.order(:end_time).last
-    end
+    last_work_experience = student.work_experiences.order(:end_time).last if student.work_experiences.any?
 
-    if last_work_experience && last_school_experience
-      if last_work_experience.end_time.to_i >= last_school_experience.end_time.to_i
-        last_work_experience.company.name
-      elsif last_work_experience.end_time.to_i <= last_school_experience.end_time.to_i
-        last_work_experience.school.name
+    last_school_experience = student.school_experiences.order(:end_time).last if student.school_experiences.any?
+
+    if last_work_experience || last_school_experience
+      if last_work_experience.try(:end_time) === "Aujourd'hui" && last_school_experience.try(:end_time) === "Aujourd'hui"
+        false
+      elsif last_work_experience.try(:end_time) === "Aujourd'hui" && !last_school_experience.try(:end_time) === "Aujourd'hui"
+        true
+      elsif !last_work_experience.try(:end_time) === "Aujourd'hui" && last_school_experience.try(:end_time) === "Aujourd'hui"
+        false
+      elsif last_work_experience.try(:end_time).to_i === last_school_experience.try(:end_time).to_i
+        true
+      elsif last_work_experience.try(:end_time).to_i >= last_school_experience.try(:end_time).to_i
+        true
+      elsif last_work_experience.try(:end_time).to_i <= last_school_experience.try(:end_time).to_i
+        false
       end
     end
-
-
   end
+
 end
+
+#True = work / False = school
+
+
+
+
+
