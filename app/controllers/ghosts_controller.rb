0bf598d0c74c2347e0eca58956fab3433c044b9f
Adrien Barbier <adrien-barbier@hotmail.fr>
2020-10-15 09:47:39 +0200

    a user can send an invitation a ghost

--- app/controllers/ghosts_controller.rb
+++ app/controllers/ghosts_controller.rb
@@ -6,6 +6,16 @@ class GhostsController < ApplicationController
 
   def send_invite
     ghost = Ghost.find(params[:id])
+    send_mail_to_ghost(ghost)
+    ghost.update(notification_send: true)
+    flash[:notice] = "Une invitation à bien été envoyé à #{ghost.full_name}."
+    redirect_to request.referrer
+  end
+
+
+  private
+
+  def send_mail_to_ghost(ghost)
     user = current_user
     UserMailer.with(ghost: ghost, user: user).send_invitation.deliver_now
   end
