76215131f91b3a320011d3e35dbd472d9a2abf7f
Adrien Barbier <adrien-barbier@hotmail.fr>
2020-10-16 10:13:28 +0200

    a user can edit or delete a list

--- app/controllers/admin/lists_controller.rb
+++ app/controllers/admin/lists_controller.rb
@@ -1,5 +1,7 @@
 class Admin::ListsController < ApplicationController
 
+  before_action :set_list, only: [:edit, :update, :destroy]
+
   def index
     @lists = List.all
   end
@@ -29,7 +31,33 @@ class Admin::ListsController < ApplicationController
     end
   end
 
+  def edit
+    @lists = List.all
+    @students = User.search(params)
+    @diplomas = Diploma.all
+    @sectors = Sector.all
+  end
+
+  def update
+    if @list.update(list_params)
+      redirect_to admin_list_path(@list)
+      flash[:notice] = "La liste #{@list.name} a bien été modifiée."
+    else
+      render :edit
+      flash[:alert] = "La liste n'a pas pu être modifiée"
+    end
+  end
+
+  def destroy
+    @list.destroy
+    redirect_to admin_lists_path
+  end
+
   private
+
+  def set_list
+    @list = List.find(params[:id])
+  end
 
   def list_params
     params.require(:list).permit(:name, user_ids: [])
--- app/helpers/lists_helper.rb
+++ app/helpers/lists_helper.rb
@@ -1,7 +1,6 @@
 module ListsHelper
 
   def email_list(list)
-
     email_string = ""
     users = list.users
 
@@ -9,8 +8,8 @@ module ListsHelper
       email_string << "#{user.email},"
     end
 
-    email_string_without_last = email_string.chop
+    email_string_without_last_c = email_string.chop
 
-    return "mailto:" + email_string_without_last
+    return "mailto:" + email_string_without_last_c
   end
 end
