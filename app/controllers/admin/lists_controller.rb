50f7ceeba712ce18dc069705727418f234a9ef1a
Adrien Barbier <adrien-barbier@hotmail.fr>
2020-10-16 08:53:36 +0200

    add collection check box to select users in lists

--- app/controllers/admin/lists_controller.rb
+++ app/controllers/admin/lists_controller.rb
@@ -8,4 +8,29 @@ class Admin::ListsController < ApplicationController
     @lists = List.all
     @list = List.find(params[:id])
   end
+
+  def new
+    @lists = List.all
+    @list = List.new
+    @students = User.search(params)
+    @diplomas = Diploma.all
+    @sectors = Sector.all
+  end
+
+  def create
+    @list = List.new(list_params)
+
+    if @list.save
+      redirect_to admin_lists_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def list_params
+    params.require(:list).permit(:name, user_ids: [])
+  end
 end
+
