ccaa8cfc4efb69063cad175219c5186fae1de64e
Adrien Barbier <adrien-barbier@hotmail.fr>
2020-10-22 14:53:21 +0200

    rethinking the student card helper

--- app/helpers/student_cards_helper.rb
+++ app/helpers/student_cards_helper.rb
@@ -1,48 +1,47 @@
 module StudentCardsHelper
   def last_activity(student)
-
-    # #True = work / False = school
-
-    # if student.work_experiences.any? && student.school_experiences.empty?
-    #   #If there is no school experiences
-    #   return true
-    # elsif student.work_experiences.empty? && student.school_experiences.any?
-    #   #If there is no work experiences
-    #   return false
-    # elsif student.work_experiences.any? && student.school_experiences.any?
-    #   #If there is work and school experiences
-    #   last_work_experience = student.work_experiences.order(:end_time).last
-    #   last_school_experience = student.school_experiences.order(:end_time).last
-
-    #   if last_work_experience.try(:end_time) === "Aujourd'hui" && last_school_experience.try(:end_time) === "Aujourd'hui"
-    #     #If the work and school experiences are actual
-    #     return true
-    #   elsif last_work_experience.try(:end_time) === "Aujourd'hui" && !last_school_experience.try(:end_time) === "Aujourd'hui"
-    #     #If the job work experiences is actual
-    #     return true
-    #   elsif !last_work_experience.try(:end_time) === "Aujourd'hui" && last_school_experience.try(:end_time) === "Aujourd'hui"
-    #     #If the school experiences is actual
-    #     return false
-    #   elsif last_work_experience.try(:end_time).to_i === last_school_experience.try(:end_time).to_i
-    #     #If the work and school experiences end at the same date
-    #     return true
-    #   elsif last_work_experience.try(:end_time).to_i >= last_school_experience.try(:end_time).to_i
-    #     #If the work experience end after the school experiences
-    #     return true
-    #   elsif last_work_experience.try(:end_time).to_i <= last_school_experience.try(:end_time).to_i
-    #     #If the school experience en after the work experiences
-    #     return false
-    #   end
-    # end
-    # if student.work_experiences.any? || student.school_experiences.any?
-    #   work_exp = student.work_experiences if student.work_experiences.any?
-    #   school_exp = student.school_experiences if student.school_experiences.any?
-    #   total_exp = work_exp + school_exp
-    #   total_exp.order(:endtime, :start_time)
-    #   binding.pry
+    current_work_exp = student.work_experiences.where(current: true)
+    current_school_exp = student.school_experiences.where(current: true)
+    work_exp_sort = student.work_experiences.order(:end_time)
+    school_exp_sort = student.school_experiences.order(:end_time)
 
 
-    # end
+    if student.work_experiences.any? && student.school_experiences.any?
+      if current_work_exp.any?
+        exp = current_work_exp.order(:start_time).last
+        return [true, exp.company.name]
+      elsif current_school_exp.any?
+        exp = current_school_exp.order(:start_time).last
+        return [false, exp.school.name]
+      elsif work_exp_sort.last.end_time == school_exp_sort.last.end_time
+        exp = work_exp_sort.last
+        return [true, exp.company.name]
+      elsif work_exp_sort.last.end_time > school_exp_sort.last.end_time
+        exp = work_exp_sort.last
+        return [true, exp.company.name]
+      else
+        exp = school_exp_sort.last
+        return [false, exp.school.name]
+      end
+    elsif student.work_experiences.any? && student.school_experiences.empty?
+      if current_work_exp.any?
+        exp = current_work_exp.order(:start_time).last
+        return [true, exp.company.name]
+      else
+        exp = work_exp_sort.last
+        return [true, exp.company.name]
+      end
+    elsif student.work_experiences.empty? && student.school_experiences.any?
+      if current_school_exp.any?
+        exp = current_school_exp.order(:start_time).last
+        return [false, exp.school.name]
+      else
+        exp = school_exp_sort.last
+        return [false, exp.school.name]
+      end
+    else
+      return ''
+    end
 
   end
 
